5d82d57f9faa15f1c95933ca92272ea666b7aa92
YeEun <ydkrkek@gmail.com>
2023-10-08 16:34:44 +0900

    [Add] Zombie 프로젝트

---
+++ Zombie/Assets/Scripts/Enemy.cs
@@ -0,0 +1,156 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.AI; // AI, 내비게이션 시스템 관련 코드를 가져오기
+
+// 적 AI를 구현한다
+public class Enemy : LivingEntity {
+    public LayerMask whatIsTarget; // 추적 대상 레이어
+
+    private LivingEntity targetEntity; // 추적할 대상
+    private NavMeshAgent pathFinder; // 경로계산 AI 에이전트
+
+    public ParticleSystem hitEffect; // 피격시 재생할 파티클 효과
+    public AudioClip deathSound; // 사망시 재생할 소리
+    public AudioClip hitSound; // 피격시 재생할 소리
+
+    private Animator enemyAnimator; // 애니메이터 컴포넌트
+    private AudioSource enemyAudioPlayer; // 오디오 소스 컴포넌트
+    private Renderer enemyRenderer; // 렌더러 컴포넌트
+
+    public float damage = 20f; // 공격력
+    public float timeBetAttack = 0.5f; // 공격 간격
+    private float lastAttackTime; // 마지막 공격 시점
+
+    // 추적할 대상이 존재하는지 알려주는 프로퍼티
+    private bool hasTarget
+    {
+        get
+        {
+            // 추적할 대상이 존재하고, 대상이 사망하지 않았다면 true
+            if (targetEntity != null && !targetEntity.dead)
+            {
+                return true;
+            }
+
+            // 그렇지 않다면 false
+            return false;
+        }
+    }
+
+    private void Awake() {
+        // 초기화
+        pathFinder = GetComponent<NavMeshAgent>();
+        enemyAnimator = GetComponent<Animator>();
+        enemyAudioPlayer = GetComponent<AudioSource>();
+
+        enemyRenderer = GetComponentInChildren<Renderer>();
+    }
+
+    // 적 AI의 초기 스펙을 결정하는 셋업 메서드
+    public void Setup(float newHealth, float newDamage, float newSpeed, Color skinColor) {
+        startingHealth = newHealth;
+        health = newHealth;
+        damage = newDamage;
+
+        pathFinder.speed = newSpeed;
+        enemyRenderer.material.color = skinColor;
+    
+    }
+
+    private void Start() {
+        // 게임 오브젝트 활성화와 동시에 AI의 추적 루틴 시작
+        StartCoroutine(UpdatePath());
+    }
+
+    private void Update() {
+        // 추적 대상의 존재 여부에 따라 다른 애니메이션을 재생
+        enemyAnimator.SetBool("HasTarget", hasTarget);
+    }
+
+    // 주기적으로 추적할 대상의 위치를 찾아 경로를 갱신
+    private IEnumerator UpdatePath() {
+        // 살아있는 동안 무한 루프
+        while (!dead)
+        {
+            if(hasTarget)
+            {
+                pathFinder.isStopped = false;
+                pathFinder.SetDestination(targetEntity.transform.position);
+                
+            }else
+            {
+                pathFinder.isStopped = true;
+
+                //whatistarget 레이어를 가진 콜라이더만 가져오도록 필터링
+                Collider [] colliders = Physics.OverlapSphere(transform.position,20f,whatIsTarget);
+
+                for(int i = 0 ; i < colliders.Length ; i++)
+                {
+                    LivingEntity livingEntity = colliders[i].GetComponent<LivingEntity>();
+                    if (livingEntity != null && !livingEntity.dead)
+                    {
+                        targetEntity = livingEntity;
+                        break;
+
+                    }
+                }
+            }
+            // 0.25초 주기로 처리 반복
+            yield return new WaitForSeconds(0.25f);
+        }
+    }
+
+    // 데미지를 입었을때 실행할 처리
+    public override void OnDamage(float damage, Vector3 hitPoint, Vector3 hitNormal) {
+        if(!dead)
+        {
+            hitEffect.transform.position = hitPoint;
+            hitEffect.transform.rotation = Quaternion.LookRotation(hitNormal);
+            hitEffect.Play();
+
+            enemyAudioPlayer.PlayOneShot(hitSound);
+        }
+        
+        // LivingEntity의 OnDamage()를 실행하여 데미지 적용
+        
+        
+        base.OnDamage(damage, hitPoint, hitNormal);
+    }
+
+    // 사망 처리
+    public override void Die() {
+        // LivingEntity의 Die()를 실행하여 기본 사망 처리 실행
+        base.Die();
+
+        Collider [] enemyColliders = GetComponents<Collider>();
+        for (int i =0 ; i< enemyColliders.Length ; i++)
+        {
+            enemyColliders[i].enabled = false;
+        }
+
+        pathFinder.isStopped =true;
+        pathFinder.enabled = false;
+
+        enemyAnimator.SetTrigger("Die");
+        enemyAudioPlayer.PlayOneShot(deathSound);
+    }
+
+    private void OnTriggerStay(Collider other) {
+        // 트리거 충돌한 상대방 게임 오브젝트가 추적 대상이라면 공격 실행 
+
+        if(!dead && Time.time >= lastAttackTime + timeBetAttack) 
+        {
+            LivingEntity attackTarget = other.GetComponent<LivingEntity>();
+
+            if(attackTarget != null && attackTarget == targetEntity)
+            {
+                lastAttackTime = Time.time;
+
+                Vector3 hitPoint = other.ClosestPoint(transform.position);
+                Vector3 hitNormal = transform.position - other.transform.position;
+
+                attackTarget.OnDamage(damage,hitPoint,hitNormal);
+            }
+        }
+    }
+}
---
+++ Zombie/Assets/Scripts/Gun.cs
@@ -0,0 +1,167 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngineInternal;
+
+// 총을 구현한다
+public class Gun : MonoBehaviour {
+    // 총의 상태를 표현하는데 사용할 타입을 선언한다
+    public enum State {
+        Ready, // 발사 준비됨
+        Empty, // 탄창이 빔
+        Reloading // 재장전 중
+    }
+
+    public State state { get; private set; } // 현재 총의 상태
+
+    public Transform fireTransform; // 총알이 발사될 위치
+
+    public ParticleSystem muzzleFlashEffect; // 총구 화염 효과
+    public ParticleSystem shellEjectEffect; // 탄피 배출 효과
+
+    private LineRenderer bulletLineRenderer; // 총알 궤적을 그리기 위한 렌더러
+
+    private AudioSource gunAudioPlayer; // 총 소리 재생기
+    public AudioClip shotClip; // 발사 소리
+    public AudioClip reloadClip; // 재장전 소리
+
+    public float damage = 25; // 공격력
+    private float fireDistance = 50f; // 사정거리
+
+    public int ammoRemain = 100; // 남은 전체 탄약
+    public int magCapacity = 25; // 탄창 용량
+    public int magAmmo; // 현재 탄창에 남아있는 탄약
+
+
+    public float timeBetFire = 0.12f; // 총알 발사 간격
+    public float reloadTime = 1.8f; // 재장전 소요 시간
+    private float lastFireTime; // 총을 마지막으로 발사한 시점
+
+
+    private void Awake() {
+        // 사용할 컴포넌트들의 참조를 가져오기
+        gunAudioPlayer=GetComponent<AudioSource>();
+        bulletLineRenderer = GetComponent<LineRenderer>();
+
+
+        bulletLineRenderer.positionCount = 2;
+        bulletLineRenderer.enabled = false;
+
+    }
+
+    private void OnEnable() {
+        // 총 상태 초기화
+
+        magAmmo = magCapacity;
+        state = State.Ready;
+        //탄창채우고 총을 쏠상태로 변경
+        lastFireTime = 0;
+        // 총쏜시점 초기화
+    }
+
+    // 발사 시도
+    public void Fire() {
+
+        //준비상태일때 마지막 총 발사시점에서 timebetFire이상의 시간이 지남
+        if(state == State.Ready && Time.time >= lastFireTime+timeBetFire)
+        {
+            lastFireTime = Time.time;
+            //마지막 총 발사시점 갱신
+            Shot();
+            // 발사
+
+        }
+
+    }
+
+    // 실제 발사 처리
+    private void Shot() {
+
+        RaycastHit hit;
+        Vector3 hitPosition = Vector3.zero;
+
+        //out 키워드는 변경사항을 유지한채로 인풋값을 내뱉음
+        if(Physics.Raycast(fireTransform.position, fireTransform.forward, out hit, fireDistance))
+        {
+            IDamageable target = hit.collider.GetComponent<IDamageable>();
+
+            if(target != null)
+            {
+                target.OnDamage(damage,hit.point, hit.normal);
+            }
+
+            hitPosition = hit.point;
+
+        }
+
+        else
+        {
+            hitPosition = fireTransform.position + fireTransform.forward *fireDistance;
+        }
+        
+
+        StartCoroutine(ShotEffect(hitPosition));
+
+        magAmmo--;
+        if(magAmmo<=0)
+        {
+            state = State.Empty;
+        }
+    }
+
+    // 발사 이펙트와 소리를 재생하고 총알 궤적을 그린다
+    private IEnumerator ShotEffect(Vector3 hitPosition) {
+
+        muzzleFlashEffect.Play();
+        shellEjectEffect.Play();
+
+        gunAudioPlayer.PlayOneShot(shotClip);
+        bulletLineRenderer.SetPosition(0,fireTransform.position);
+        bulletLineRenderer.SetPosition(1,hitPosition);
+
+
+        // 라인 렌더러를 활성화하여 총알 궤적을 그린다
+        bulletLineRenderer.enabled = true;
+
+        // 0.03초 동안 잠시 처리를 대기
+        yield return new WaitForSeconds(0.03f);
+
+        // 라인 렌더러를 비활성화하여 총알 궤적을 지운다
+        bulletLineRenderer.enabled = false;
+    }
+
+    // 재장전 시도
+    public bool Reload() {
+
+        if(state==State.Reloading || ammoRemain <= 0 || magAmmo >= magCapacity)
+        {
+            return false;
+
+        }
+
+        StartCoroutine(ReloadRoutine());
+        return true;
+    }
+
+    // 실제 재장전 처리를 진행
+    private IEnumerator ReloadRoutine() {
+        // 현재 상태를 재장전 중 상태로 전환
+        state = State.Reloading;
+        
+        gunAudioPlayer.PlayOneShot(reloadClip);
+        // 재장전 소요 시간 만큼 처리를 쉬기
+        yield return new WaitForSeconds(reloadTime);
+        int ammoToFill = magCapacity - magAmmo;
+
+        if(ammoRemain < ammoToFill)
+        {
+            ammoToFill = ammoRemain;
+        }
+
+        magAmmo += ammoToFill;
+        ammoRemain -= ammoToFill;
+        
+        // 총의 현재 상태를 발사 준비된 상태로 변경
+        state = State.Ready;
+    }
+}
